a9593de4f6cf9ac09a6aeb363f3186f9774974e6
Gabriel Roec <you@example.com>
2023-01-09 19:17:46 -0300

    update lessons

--- 28-non-null-assertion-operator/index.js
+++ 28-non-null-assertion-operator/index.js
@@ -1,7 +1,3 @@
 //Non-null Assertion Operator
 var p = document.getElementById("some-p");
 console.log(p.innerText);
-p.addEventListener("click", changeTextWithClick);
-function changeTextWithClick() {
-    p.innerText = "Hello, Dude";
-}
--- 28-non-null-assertion-operator/index.ts
+++ 28-non-null-assertion-operator/index.ts
@@ -1,10 +1,4 @@
 //Non-null Assertion Operator
 
-const p = document.getElementById("some-p")
-console.log(p!.innerText)
-
-
-p!.addEventListener("click",changeTextWithClick)
-function changeTextWithClick(){
-  p!.innerText="Hello, Dude"
-}
+const p = document.getElementById("some-p");
+console.log(p!.innerText);
