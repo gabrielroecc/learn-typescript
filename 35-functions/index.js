ba27325a59a8d532211cb12a9227abb734dcc7f4
Gabriel Roec <you@example.com>
2023-01-16 20:24:08 -0300

    Learning Callback functions

--- 35-functions/index.js
+++ 35-functions/index.js
@@ -1,48 +1,52 @@
 // 1 - Void
-function withoutReturn() {
-    console.log("This function don't have return");
-}
-// 2 - Callback /////////////////////////////////////////
-function myFunction() {
-    //this is a normal function
-    //Do Something
-    console.log("My funtion executed");
-}
-var myFunctionInsideAVariable = function () {
-    //This is a exalmple of funciton expression, function inside a variable
-    //Do something
-    console.log("My funtion executed");
+// function withoutReturn(): void {
+//   console.log("This function don't have return");
+// }
+// // 2 - Callback /////////////////////////////////////////
+// function myFunction() {
+//   //this is a normal function
+//   //Do Something
+//   console.log("My funtion executed");
+// }
+// const myFunctionInsideAVariable = function () {
+//   //This is a exalmple of funciton expression, function inside a variable
+//   //Do something
+//   console.log("My funtion executed");
+// };
+// const myNamedFunctionInsideAVariable = function myNamedFunction() {
+//   //This also is a exalmple of funciton expression, named function inside a variable
+//   //Do something
+//   console.log("My funtion executed");
+// };
+// const myArrowFunction = () => {
+//   //This is a example of arrow function
+//   //Do something
+//   console.log("My funtion executed");
+// };
+// //Obs#1 - Function recives arguments and returns values
+// const myFunction1 = (message: string) => {
+//   //do something
+//   console.log("My executed function", message);
+//   return "My executed function" + message;
+// };
+// myFunction1("Hello, Gab");
+// const multiply = (number: number) => {
+//   //do something
+//   console.log(number * number);
+//   return number * number;
+// };
+// multiply(5);
+// function createPerson(name: string) {
+//   return name;
+// }
+// const person = createPerson; //I put a function inside a variable, now, person is a function
+// const person2 = createPerson("Gab"); //I put a function with argument inside a variable, now, person2 ia a function with the argument assined
+var saudar = function (nome, responder) {
+    var horaAtual = 12;
+    // const responder = (saudacao: string) => console.log(saudacao + nome);
+    if (horaAtual <= 12)
+        return responder("Bom dia, " + nome);
 };
-var myNamedFunctionInsideAVariable = function myNamedFunction() {
-    //This also is a exalmple of funciton expression, named function inside a variable
-    //Do something
-    console.log("My funtion executed");
-};
-var myArrowFunction = function () {
-    //This is a example of arrow function
-    //Do something
-    console.log("My funtion executed");
-};
-//Obs#1 - Function recives arguments and returns values
-var myFunction1 = function (message) {
-    //do something
-    console.log("My executed function", message);
-    return "My executed function" + message;
-};
-myFunction1("Hello, Gab");
-var multiply = function (number) {
-    //do something
-    console.log(number * number);
-    return number * number;
-};
-multiply(5);
-var greet = function (name) {
-    var atualTime = new Date("July 21, 1983 01:15:00").getHours();
-    if (atualTime <= 12)
-        return "Good Morning" + name;
-    if (atualTime <= 18)
-        return "Good Afternoon" + name;
-    return "Good Night";
-};
-greet("Gab");
+var responderCallback = function (saudacao) { return console.log(saudacao); };
+saudar("Gab", responderCallback);
 /////////////////////////////////////////////
--- 35-functions/index.ts
+++ 35-functions/index.ts
@@ -1,55 +1,63 @@
 // 1 - Void
-function withoutReturn(): void {
-  console.log("This function don't have return");
-}
+// function withoutReturn(): void {
+//   console.log("This function don't have return");
+// }
 
-// 2 - Callback /////////////////////////////////////////
+// // 2 - Callback /////////////////////////////////////////
 
-function myFunction() {
-  //this is a normal function
-  //Do Something
-  console.log("My funtion executed");
-}
+// function myFunction() {
+//   //this is a normal function
+//   //Do Something
+//   console.log("My funtion executed");
+// }
 
-const myFunctionInsideAVariable = function () {
-  //This is a exalmple of funciton expression, function inside a variable
-  //Do something
-  console.log("My funtion executed");
+// const myFunctionInsideAVariable = function () {
+//   //This is a exalmple of funciton expression, function inside a variable
+//   //Do something
+//   console.log("My funtion executed");
+// };
+
+// const myNamedFunctionInsideAVariable = function myNamedFunction() {
+//   //This also is a exalmple of funciton expression, named function inside a variable
+//   //Do something
+//   console.log("My funtion executed");
+// };
+
+// const myArrowFunction = () => {
+//   //This is a example of arrow function
+//   //Do something
+//   console.log("My funtion executed");
+// };
+
+// //Obs#1 - Function recives arguments and returns values
+
+// const myFunction1 = (message: string) => {
+//   //do something
+//   console.log("My executed function", message);
+//   return "My executed function" + message;
+// };
+// myFunction1("Hello, Gab");
+
+// const multiply = (number: number) => {
+//   //do something
+//   console.log(number * number);
+//   return number * number;
+// };
+
+// multiply(5);
+
+// function createPerson(name: string) {
+//   return name;
+// }
+// const person = createPerson; //I put a function inside a variable, now, person is a function
+// const person2 = createPerson("Gab"); //I put a function with argument inside a variable, now, person2 ia a function with the argument assined
+
+const saudar = (nome: string, responder: (string) => void) => {
+  const horaAtual = 12;
+  if (horaAtual <= 12) return responder("Bom dia, " + nome);
 };
-
-const myNamedFunctionInsideAVariable = function myNamedFunction() {
-  //This also is a exalmple of funciton expression, named function inside a variable
-  //Do something
-  console.log("My funtion executed");
-};
-
-const myArrowFunction = () => {
-  //This is a example of arrow function
-  //Do something
-  console.log("My funtion executed");
-};
-
-//Obs#1 - Function recives arguments and returns values
-
-const myFunction1 = (message: string) => {
-  //do something
-  console.log("My executed function", message);
-  return "My executed function" + message;
-};
-myFunction1("Hello, Gab");
-
-const multiply = (number: number) => {
-  //do something
-  console.log(number * number);
-  return number * number;
-};
-
-multiply(5);
-
-function createPerson(name: string) {
-  return name;
-}
-const person = createPerson; //I put a function inside a variable, now, person is a function
-const person2 = createPerson("Gab"); //I put a function with argument inside a variable, now, person2 ia a function with the argument assined
-
+const responderCallback = (saudacao: string) => console.log(saudacao);
+saudar("Gab", responderCallback);
 /////////////////////////////////////////////
+
+//Generic Functions
